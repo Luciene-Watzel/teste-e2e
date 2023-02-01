3916eabd12fd99751fac6bd5b12ec6313ae7cf81
Luciene Watzel <lu.watzel@gmail.com>
2023-02-01 20:46:32 -0300

    Teste e2e

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+var faker = require('faker');
+
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
@@ -10,12 +12,38 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('/')
-    });
+        cy.visit('http://lojaebac.ebaconline.art.br/')
 
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
     });
 
 
-})
+    //Deve fazer um pedido na loja Ebac Shop de ponta a ponta
+
+    it('Deve adicionar porduto ao carrinho - Usando comando customizado', () => {
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+        cy.addProdutos('.post-2559 > .product-block > .block-inner > .image > .product-image > .image-hover', 'M', 'Blue', 4)
+
+    //Checkout
+        cy.get('#billing_first_name').type(faker.name.firstName())
+        cy.get('#billing_last_name').type(faker.name.lastName())
+        cy.get('#select2-billing_country-container').click().type('Brasil').get('[aria-selected="true"]').click()
+        cy.get('#billing_address_1').type('Lauzane, 30')
+        cy.get('#billing_city').type('São Paulo')
+        cy.get('#select2-billing_state-container').click().type('São Paulo' + '{Enter}')
+        cy.get('#billing_postcode').type('04788888')
+        cy.get('#billing_phone').type('25380000')
+        cy.get('#billing_email').type(faker.internet.email())
+        cy.get('#createaccount').click()
+        cy.get('#account_password').type('Lu12228990**')
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+        cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+
+
+    });
+
+
+
+
+});
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,9 +25,22 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 
+
+
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
+    cy.get('#password').type(senha, { log: false })
     cy.get('.woocommerce-form > .button').click()
 });
+
+Cypress.Commands.add('addProdutos', (produto, tamanho, cor, quantidade) => {
+    
+    cy.get(produto).click()
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get('.button-variable-item-' + cor).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+    cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+    cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+})
 
